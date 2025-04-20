2b70daf7a077c8b516f345da282a6ae1f7b67241
Riya <riya01022001@gmail.com>
2025-04-20 15:23:51 +0530

    update codebase

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const app = express();
 app.use(express.json());
 const corsOptions = {
-  origin: 'https://frontend-nu-wine.vercel.app/',
+  origin: 'https://frontend-nu-wine.vercel.app',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
