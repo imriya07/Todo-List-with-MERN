87bd8afad49ee1405ed029d40c25f3476556d415
Riya <riya01022001@gmail.com>
2025-04-20 15:04:06 +0530

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const app = express();
 app.use(express.json());
 const corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: 'https://frontend-nu-wine.vercel.app/',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
@@ -26,7 +26,9 @@ app.use('/api/auth', auth);
 app.use('/api/tasks',tasks)
 
 const PORT = process.env.PORT || 8000;
-app.listen(PORT,() =>{
-  console.log(`server running ${PORT}`);
-})
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => {
+    console.log(`server running ${PORT}`);
+  });
+}
 module.exports = app;
